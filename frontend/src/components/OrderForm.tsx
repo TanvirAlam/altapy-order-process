fc92f6955f5c836cb6c9d1bb0f06eecac98503f5
TAlam <tanvir.alam.shawn@gmail.com>
2021-11-19 01:48:43 +0100

    OrderFomr redesign

--- frontend/src/components/OrderForm.tsx
+++ frontend/src/components/OrderForm.tsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
+import { styled } from '@mui/material/styles';
 import Box from "@mui/material/Box";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import axiosRequest from "../services/axios-request";
 import INewOrder from "../types/newOrder.type";
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from '@mui/material/InputAdornment';
+import Collapse from '@mui/material/Collapse';
 
 const OrderForm: React.FC = () => {
     const [code, setCode] = useState('');
@@ -11,6 +17,7 @@ const OrderForm: React.FC = () => {
     const [quantity, setQuantity] = useState(0);
     const [price, setPrice] = useState(0);
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isFromValidated, setFromValidated] = useState(false);
 
     const handleSubmitForm = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -34,6 +41,13 @@ const OrderForm: React.FC = () => {
             });
     }
 
+    const Item = styled(Paper)(({ theme }) => ({
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+
     return (
         <div>
             <Box component="form" noValidate autoComplete="off" onSubmit={handleSubmitForm} sx={{
@@ -48,71 +62,67 @@ const OrderForm: React.FC = () => {
                 padding: '2rem',
                 '& .MuiTextField-root': { m: 1, width: '25ch' },
             }}>
-                <div>
-                    <TextField
-                        id="code"
-                        label="Code"
-                        multiline
-                        maxRows={4}
-                        value={code}
-                        onChange={(
-                            ev: React.ChangeEvent<HTMLInputElement>,
-                        ): void => {
-                            setCode(ev.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="description"
-                        label="Description"
-                        multiline
-                        maxRows={4}
-                        value={description}
-                        onChange={(
-                            ev: React.ChangeEvent<HTMLInputElement>,
-                        ): void => {
-                            setDescription(ev.target.value);
-                        }}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="quantity"
-                        label="Quantity"
-                        type="number"
-                        multiline
-                        maxRows={4}
-                        value={quantity}
-                        onChange={(
-                            ev: React.ChangeEvent<HTMLInputElement>,
-                        ): void => {
-                            setQuantity(
-                                parseInt(ev.target.value, 10),
-                            );
-                        }}
-                    />
-                </div>
-                <div>
-                    <TextField
-                        id="price"
-                        label="Price"
-                        type="number"
-                        multiline
-                        maxRows={4}
-                        value={price}
-                        onChange={(
-                            ev: React.ChangeEvent<HTMLInputElement>,
-                        ): void => {
-                            setPrice(
-                                parseInt(ev.target.value, 10),
-                            );
-                        }}
-                    />
-                </div>
-                <div>
-                    <Button variant="outlined" type="submit">Create</Button>
-                </div>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={6}>
+                        <Item sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+                            <TextField
+                                id="code"
+                                label="Code"
+                                value={code}
+                                style={{width: '98%'}}
+                                helperText="eg: 'ABCDFED1213'"
+                            />
+                            <IconButton color="inherit" onClick={() => console.log('sdfsdf')}>
+                                Generate
+                            </IconButton>
+                        </Item>
+
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item>
+                            <TextField
+                                id="description"
+                                label="Description"
+                                multiline
+                                maxRows={4}
+                                value={description}
+                                style={{width: '98%'}}
+                            />
+                        </Item>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item>
+                            <TextField
+                                id="quantity"
+                                label="Quantity"
+                                type="number"
+                                value={quantity}
+                                style={{width: '98%'}}
+                            />
+                        </Item>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Item>
+                            <TextField
+                                id="price"
+                                label="Price"
+                                type="number"
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                }}
+                                style={{width: '98%'}}
+                                value={price}
+                            />
+                        </Item>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Collapse in={isFromValidated}>
+                            <Item>
+                                <Button variant="outlined" type="submit">Create</Button>
+                            </Item>
+                        </Collapse>
+                    </Grid>
+                </Grid>
             </Box>
         </div>
     );
